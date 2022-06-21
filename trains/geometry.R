b8d5b547ef91c9d75becf5eb389a9b290bf60dd6
Jonathan Dushoff <dushoff@mcmaster.ca>
2022-06-21 10:55:16 -0400

    some stuff is working; time to refactor a bit

--- trains/geometry.R
+++ trains/geometry.R
@@ -7,15 +7,25 @@ empCV <- function(r){
 	return(sum(res^2)/sum(res)^2)
 }
 
-## sum/sum^2
-## (rho^(2n)-1)/(rho^2-1)) / (rho^(n)-1)/(rho-1))^2 / 
-## (ρ-1)(ρ^n+1)/((ρ+1)(ρ^n-1))
-
-
-
-dumbCV <- function(b, rho){
-	return((rho-1)*(rho^b+1)/(rho+1))
-}
+## sum(v^2)/sum(v)^2
+## Tried the math; even the forward math looks inaesthetic
 
 empCV(geomRates(4, 2))
-dumbCV(4, 2)
+
+cvCheck <- function(rho, c, boxes){
+	return(empCV(geomRates(boxes, rho)) - c)
+}
+
+cvr <- function(c, boxes, minrat=0.01){
+	if(cvCheck(1, c, boxes) > 0) {stop("More boxes needed in cvr")}
+	if(cvCheck(minrat, c, boxes) < 0) {stop("CV too high in cvr (reduce minrat if necessary)")}
+	return(
+		uniroot(cvCheck
+			, c(minrat, 1)
+			, c, boxes
+		)$root
+	)
+}
+
+cvr(0.26, 4)
+cvr(0.25, 4)
