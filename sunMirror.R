a8f3aefa3dd294aab8f392357d93256565a64bfc
Jonathan Dushoff <dushoff@mcmaster.ca>
2024-03-15 17:43:27 -0400

    Leaving office

--- sunMirror.R
+++ sunMirror.R
@@ -1,17 +1,29 @@
 
+xlab="\u2113"
+
+print(xlab)
+
 deg <- pi/180
 refract <- 1.33
 travel <- 45*deg
 sun <- 27.6*deg
 
+## What is the angle of the incident beam (striking the water) from the vertical?
+## This is the answer that Miriam got
+## sin(snell) = refract*sin(travel)
 snell <- asin(refract*sin(travel))
+print(snell/deg)
 
-## print(snell/deg)
+## Incident vs. horizontal
+incident <- 90*deg - snell
+print(incident/deg)
 
-normal <- mean(c(sun, snell-90*deg))
+## The normal vector to the mirror is halfway between the sunbeam angle and the incident angle
+## But the reflected beam could keep going past the raft, or be reflected back to where it came from!
+## We assume it's being reflected back.
+## (28-20)/2
+normal <- (sun - incident)/2
 
-mirror <- 90*deg+normal
+answer <- 90*deg + normal
 
-print(mirror/deg)
-
-
+print(answer/deg)
