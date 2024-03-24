0aba023bdb3bf6e7ef00fc88701cd490ffa3a7d7
Jonathan Dushoff <dushoff@mcmaster.ca>
2024-03-24 15:19:26 -0400

    leaving living room

--- jdPlots.R
+++ jdPlots.R
@@ -3,9 +3,32 @@ spot <- read.csv("spotify.csv")
 names(spot)
 attach(spot)
 
-summary(lm(in_apple_charts~in_spotify_charts))
+colors <- c("red", "blue")
+
+bhist <- hist(bpm)
+
+bpm_group <- cut(bpm, bhist$breaks)
+print(bpm_group)
+levels(bpm_group) <- bhist$mids
+bpm_mid <- as.numeric(bpm_group)
+
+bpm_mode_table <- table(mode, bpm_group)
+
+print(bpm_mode_table)
+
+barplot(bpm_mode_table, 
+        beside = FALSE, 
+        col = colors, 
+        ylab = "Number of Songs", 
+        xlab = "BPM", 
+		  ylim = c(0, 140),
+        main = "Distribution of BPM by Mode"
+
+)
 
 quit()
+
+summary(lm(in_apple_charts~in_spotify_charts))
 
 mtab <- table(released_month)
 print(mtab)
@@ -13,16 +36,15 @@ month <- as.numeric(names(mtab))
 monthly_releases <- as.numeric(mtab)
 plot(
 	month, monthly_releases
-	, type="o"
+	, type="b"
+	, ylim = c(0, max(monthly_releases))
 )
 
-boxplot(liveness_percent ~ released_month)
+boxplot(energy_percent ~ released_month)
 
 plot(
 	valence_percent, danceability_percent
 )
-
-hist(bpm)
 
 quit()
 
