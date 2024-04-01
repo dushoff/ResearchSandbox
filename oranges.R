53e6b1a0056daf21bbef18a652544b051faabc8b
Jonathan Dushoff <dushoff@mcmaster.ca>
2024-03-31 22:58:28 -0400

    leaving attic

--- oranges.R
+++ oranges.R
@@ -1,4 +1,20 @@
 
+p1 <- 746/1358 
+p2 <- 967/1379
+
+pool <- sqrt(p1*(1-p1)/1358 + p2*(1-p2)/1379)
+print(pool)
+print(pool*qnorm(0.95))
+
+print (p1-p2+pool*qnorm(0.95))
+print (p1-p2-pool*qnorm(0.95))
+
+## Sorry for the weird lazy part about 10
+d <- c(14, 8, 9, 1, 10, 5, -5, -2)
+10* sd(d)/sqrt(length(d))
+
+quit()
+
 70/100 - 70*0.01
 p <- 0.1
 n <- 125
