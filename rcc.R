58d8294ce617607267a7c2e8b33a4d55b94d6fab
Jonathan Dushoff <dushoff@mcmaster.ca>
2025-05-22 15:33:51 -0400

    leaving lab

--- rcc.R
+++ rcc.R
@@ -1,8 +1,24 @@
 
-forward <- function(mu, p, N){
+forward <- function(mu, p, S){
+	N <- S/p
 	return((mu*sqrt(p)/2)*(N-1)/(p*N-1))
 }
 
-simple <- function(L, mu, P){
-	return(2*L/
+simple <- function(L, mu, S){
+	return((mu/(2*L))^2)
 }
+
+good <- function(L, mu, S){
+	return(uniroot(χ))
+}
+
+mu <- 2e-4
+p <- 0.2
+
+L <- forward(mu, p, S=200)
+simple(L, mu, S=200)
+
+L <- forward(mu, p, S=20)
+simple(L, mu, S=20)
+
+
