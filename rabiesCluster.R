08f42587873cff1c61ba9ccf89e1fb47fe037960
Jonathan Dushoff <dushoff@mcmaster.ca>
2025-06-24 13:23:48 +0200

    Muizenberg

--- rabiesCluster.R
+++ rabiesCluster.R
@@ -1,49 +1,51 @@
 library(dplyr)
 
-numVillages <- 20
-medianDogs <- 1000
-spread <- 0.4
+rsBeta <- function(n, prob, spread){
+	if (spread==0) return(rep(prob, n))
+	b1 = (1-prob)*spread
+	b2 = prob*spread
+	return(rbeta(n, 1/b1, 1/b2))
+}
 
-vaxPropT <- 0.75
-vaxPropC <- 0.1
-vaxSpread <- 0.1
+doTrial <- function(
+	numVillages, medianDogs, spread 
+	, vaxPropT, vaxPropC, vaxSpread 
+	, rProbT, rProbC, rSpread 
+){
+	## Set up treatment
+	numTreat <- round(numVillages/2)
+	treatments <- c(rep("Vax", numTreat), rep("Control", numVillages-numTreat))
 
-rProbT <- 0.03
-rProbC <- 0.01
-rSpread <- 1
+	latent <- tibble(
+		pop = as.integer(rlnorm(numVillages
+			, meanlog=log(medianDogs), sdlog=spread)
+		)
+		, treat = sample(treatments)
+		, vpbase = ifelse(treat=="Vax", vaxPropT, vaxPropC)
 
-## Set up treatment
-numTreat <- round(numVillages/2)
-treatments <- c(rep("Vax", numTreat), rep("Control", numVillages-numTreat))
+		## Beta calculations (follow up with me later)
+		, vp = rsBeta(numVillages, vpbase, vaxSpread)
+		, vax = rbinom(numVillages, size=pop, prob=vp)
+		, unvax = pop-vax
 
-latent <- tibble(
-	pop = as.integer(rlnorm(numVillages, meanlog=log(medianDogs), sdlog=spread))
-	, treat = sample(treatments)
-	, vpbase = ifelse(treat=="Vax", vaxPropT, vaxPropC)
-	## Beta calculations (follow up with me later)
-	, b1 = (1-vpbase)*vaxSpread
-	, b2 = vpbase*vaxSpread
-	, vp = rbeta(numVillages, 1/b1, 1/b2)
+		## Beta calculations for rabies
+		, rpBase = ifelse(treat=="Vax", rProbT, rProbC)
+		, rp = rsBeta(numVillages, rpBase, rSpread)
+		, rabies = rbinom(numVillages, size=unvax, prob=rp)
+	)
 
-	, vax = rbinom(numVillages, size=pop, prob=vp)
-	, unvax = pop-vax
+	## Lot of complications could be added here!!
+	obs <- (latent
+		|> select(treat, vax, unvax, rabies)
+	)
 
-	## Beta calculations for rabies
-	, rpBase = ifelse(treat=="Vax", rProbT, rProbC)
-	, rps1 = (1-rpBase)*rSpread
-	, rps2 = rpBase*rSpread
-	, rp = rbeta(numVillages, 1/rps1, 1/rps2)
-	, rabies = rbinom(numVillages, size=unvax, prob=rp)
-)
+	est <- glm(rabies/unvax ~ treat, weights=unvax, data=obs, family=binomial())
 
-## Lot of complications could be added here!!
-obs <- (latent
-	|> select(treat, vax, unvax, rabies)
-)
+	return(exp(confint(est)["treatVax", ]))
+}
 
-library(ggplot2); theme_set(theme_minimal())
-
-print(ggplot(obs)
-	+ aes(treat, rabies/vax)
-	+ geom_boxplot()
-)
+print(doTrial(
+	numVillages = 20, medianDogs = 1000, spread = 0.4
+	, vaxPropT = 0.75, vaxPropC = 0.1, vaxSpread = 0.1
+	, rProbT = 0.01, rProbC = 0.05 , rSpread = 1
+))
